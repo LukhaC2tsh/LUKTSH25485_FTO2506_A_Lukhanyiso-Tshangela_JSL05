cca530201e77e699da6fcf537875667fb6744bb9
LukhaC2tsh <chulumancolt@gmail.com>
2025-09-21 03:53:47 +0200

    adds created functions createTaskCard and renderTasks

--- scripts/render.js
+++ scripts/render.js
@@ -1,1 +1,40 @@
 /**Rendering the UI */
+
+const toDoCol = document.getElementById(`.column-div[data-status = "todo"] .tasks-container`);
+const doingCol = document.getElementById(`.column-div[data-status = "doing"] .tasks-container`);
+const doneCol = document.getElementById(`.column-div[data-status = "done"] .tasks-container`);
+
+/**creates a task card div and returns the card element*/
+function createTaskCard(task){
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('task-item');
+    taskCard.id = task.id;
+    taskCard.innerHTML = `${task.title}`;
+
+    return taskCard
+}
+/**places cards in the appropriate column */
+function renderTasks(tasks){
+    const card = createTaskCard(task);
+    /**clear all tasks */
+    if(toDoCol){
+        toDoCol.innerHTML = "";
+    }
+    if(doingCol){
+        doingCol.innerHTML = "";
+    }
+    if(doneCol){
+        doneCol.innerHTML = "";
+    }
+
+    /**render all tasks */
+    if(toDoCol){
+        toDoCol.appendChild(card);
+    }
+    if(doingCol){
+        doingCol.appendChild(card);
+    }
+    if(doneCol){
+        doneCol.appendChild(card);
+    }
+}
